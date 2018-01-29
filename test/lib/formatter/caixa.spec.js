c03924ff6ab2f0edec20f575eed4569d70a9e294
Leonardo Thibes <leonardothibes@gmail.com>
2018-01-29 12:06:13 -0200

    Escrevendo testes

--- test/lib/formatter/caixa.spec.js
+++ test/lib/formatter/caixa.spec.js
@@ -3,6 +3,17 @@
 const format = require('../../../lib/formatter/caixa'),
       assert = require('unit.js');
 
+const concursos = [
+    {
+        input   : '012345678',
+        expected: 12345678,
+    },
+    {
+        input   : '01.23.456.78',
+        expected: 12345678,
+    },
+];
+
 const numeros = [
     {
         input   : '012345678',
@@ -14,13 +25,58 @@ const numeros = [
     },
 ];
 
+const valores = [
+    {
+        input   : '700.000,00',
+        expected: '700000.00',
+    },
+    {
+        input   : '1.700.000,00',
+        expected: '1700000.00',
+    },
+];
+
+const datas = [
+    {
+        input   : '01/02/2018',
+        expected: '2018-02-01',
+    },
+];
+
 describe('Formatter: caixa', () =>
 {
+    concursos.forEach(concurso =>
+    {
+        it(`concurso: ${concurso.input} => ${concurso.expected}`, done =>
+        {
+            assert.number(format.concurso(concurso.input)).isEqualTo(concurso.expected);
+            done();
+        });
+    });
+
     numeros.forEach(numero =>
     {
-        it(`concurso: ${numero.input} => ${numero.expected}`, done =>
+        it(`numero: ${numero.input} => ${numero.expected}`, done =>
         {
             assert.string(format.numero(numero.input)).isEqualTo(numero.expected);
+            done();
+        });
+    });
+
+    valores.forEach(valor =>
+    {
+        it(`valor: ${valor.input} => ${valor.expected}`, done =>
+        {
+            assert.string(format.valor(valor.input)).isEqualTo(valor.expected);
+            done();
+        });
+    });
+
+    datas.forEach(data =>
+    {
+        it(`data: ${data.input} => ${data.expected}`, done =>
+        {
+            assert.string(format.data(data.input)).isEqualTo(data.expected);
             done();
         });
     });
