80b768b6b95c986f7711dd21bb78ecc04eee114d
Leonardo Thibes <leonardothibes@gmail.com>
2018-02-02 11:28:03 -0200

    Ajustando os testes dos providers

--- test/lib/provider/g1.spec.js
+++ test/lib/provider/g1.spec.js
@@ -5,54 +5,22 @@ const provider = require('../../../lib/provider/g1'),
 
 describe('Provider: g1', () =>
 {
-    it('Sucesso', done =>
+    it('URL', done =>
     {
-        let promise = provider.fetch();
-        promise.then(response =>
-        {
-            assert.object(response)
-                .hasProperty('origem')
-                .hasProperty('concurso')
-                .hasProperty('data')
-                .hasProperty('premios');
-
-            assert.string(response.origem).isEqualTo('g1');
-            assert.number(response.concurso);
-            assert.bool(RegExp(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/).test(response.data)).isTrue();
-
-            assert.object(response.premios).hasLength(5);
-            response.premios.map((sorteio, i) =>
-            {
-                assert.object(sorteio)
-                    .hasLength(3)
-                    .hasProperty('premio')
-                    .hasProperty('numero')
-                    .hasProperty('valor');
-
-                assert.number(sorteio.premio).isEqualTo(i+1);
-                assert.bool(RegExp(/^[0-9]{5}$/).test(sorteio.numero)).isTrue();
-                assert.bool(RegExp(/^[0-9]{1,7}\.[0-9]{2}$/).test(sorteio.valor)).isTrue();
-            });
-
-            done();
-        });
+        const regex = RegExp(/^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/);
+        assert.bool(regex.test(provider.url)).isTrue();
+        done();
     });
 
-    it('Falha', done =>
+    it('Headers', done =>
     {
-        provider.url = 'http://localhost';
-        let promise  = provider.fetch();
+        assert.object(provider.headers)
+            .hasProperty('Cache-Control')
+            .hasProperty('cookie');
 
-        promise.catch(error =>
-        {
-            assert.object(error)
-                .hasProperty('status')
-                .hasProperty('message');
+        assert.string(provider.headers['Cache-Control']).isEqualTo('no-cache');
+        assert.string(provider.headers.cookie).isEqualTo('security=true');
 
-            assert.number(error.status);
-            assert.string(error.message);
-
-            done();
-        });
+        done();
     });
 });
